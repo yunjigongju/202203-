fa4015382c990e561623eaf14bf34b06c7040c0a
yunjigongju <uio7456@gmail.com>
2022-03-22 22:05:56 +0900

    파이썬 3일차

---
+++ ex2.py
@@ -0,0 +1,9 @@
+def func1():
+    return "모듈의 첫번째  함수입니다."
+    
+def func2() :
+    return "모듈의 두번째 함수입니다."
+
+def func3() :
+    return "모듈의 세번째 함수입니다."
+
---
+++ ex4.py
@@ -0,0 +1,12 @@
+def func1():
+    return "모듈의 첫번째  함수입니다."
+    
+def func2() :
+    return "모듈의 두번째 함수입니다."
+
+def func3() :
+    return "모듈의 세번째 함수입니다."
+
+def func4() :
+    return "모듈의 네번째 함수입니다."
+    
